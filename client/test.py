dcc432a4a8eb9b44678fff36ce70ca25602fc943
Henri Van Oost <henri.van.oost@student.howest.be>
2021-04-23 20:28:44 +0200

    last parameter

--- client/test.py
+++ client/test.py
@@ -8,11 +8,21 @@ data = pd.read_csv("client\healthcare-dataset-stroke-data.csv")
 statistics = data["age"].describe()
 print(statistics)
 
+# parameters
+# age = data["age"].round(0)
+# print(age)
+parameters = data.round(0)
+print(parameters)
+test1 = data.loc[data.age == 1, "age"].count()
+test2 = parameters.loc[parameters.age == 1, "age"].count()
+
+print(test1)
+print(test2)
+
+
 # chart of chosen subject
 valueCounts = data["gender"].value_counts()
 valueCounts.plot.bar()
 plt.ylabel('Counts')
 plt.title("Gender")
 plt.show()
-
-# parameters
